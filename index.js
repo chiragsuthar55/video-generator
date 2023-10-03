47c84ddd7348c494155eae23d4f87cae6a104fff
Chirag Suthar <chiragsuthar.codezee@gmail.com>
2023-10-03 18:27:09 +0530

    implemented pupeeter

--- index.js
+++ index.js
@@ -202,15 +202,15 @@ app.use(function (req, res, next) {
 app.get("/api", async (req, res) => {
   let options = {};
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+  // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  //   options = {
+  //     args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+  //     defaultViewport: chrome.defaultViewport,
+  //     executablePath: await chrome.executablePath,
+  //     headless: true,
+  //     ignoreHTTPSErrors: true,
+  //   };
+  // }
 
   try {
     let browser = await puppeteer.launch(options);
